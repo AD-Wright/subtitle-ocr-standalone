e56f15bd202e8da6bc948c676e3978ed585dd36d
Mahboob Karimian <63875592+mahboobkarimian@users.noreply.github.com>
2025-02-26 18:47:22 +0100

    fix path

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ cp wrapper.sh $APPDIR/AppRun
 chmod +x $APPDIR/usr/bin/mkv-subtitle-converter
 chmod +x $APPDIR/AppRun
 
-echo `pwd`
+REPO_DIR=$(pwd)
 
 # Build and install subtile-ocr
 echo "Building subtile-ocr..."
@@ -30,7 +30,7 @@ git clone https://github.com/gwen-lg/subtile-ocr.git
 cd subtile-ocr
 echo `pwd`
 cargo build --release
-cd ../..
+cd $REPO_DIR
 cp /tmp/subtile-ocr/target/release/subtile-ocr $APPDIR/usr/lib/subtile-ocr/
 
 # Copy MKVToolNix binaries
@@ -68,9 +68,8 @@ wget -O $APPDIR/usr/share/icons/hicolor/256x256/apps/subtitleconverter.png "http
 ln -sf usr/share/applications/subtitleconverter.desktop $APPDIR/subtitleconverter.desktop
 ln -sf usr/share/icons/hicolor/256x256/apps/subtitleconverter.png $APPDIR/subtitleconverter.png
 
-cd $HOME
 # Get AppImageTool
-#wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod +x appimagetool-x86_64.AppImage
 
 # Generate AppImage
